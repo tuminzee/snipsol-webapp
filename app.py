68027feefd38bc633242812e65fbabfb43a9c5a1
tuminzee <t.sheth33@gmail.com>
2021-08-08 09:14:01 +0530

    Add demo file download link

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
+import base64
+import io
 import numpy as np
 import pandas as pd
 st.set_page_config(
-    'SNiPSOL',
+    'SNiPSoL',
     page_icon="🔬"
     )
 
-st.title('SNIPSOL')
+st.title('SNiPSoL 🔬')
 
+# st.header('Input file configuration')
+demo_file = pd.read_excel('./files/input.xlsx', dtype=str)
+# st.dataframe(demo_file)
+
+towrite = io.BytesIO()
+downloaded_file = demo_file.to_excel(towrite, encoding='utf-8', index=False, header=True)
+towrite.seek(0)  # reset pointer
+b64 = base64.b64encode(towrite.read()).decode()  # some strings
+linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="demo.xlsx">Download demo file</a>'
+st.markdown(linko, unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 placeholder = st.empty()
@@ -20,7 +30,7 @@ placeholder = st.empty()
 #     input = placeholder.text_input('text', value='clear')
 
 if uploaded_file:
-    idf = pd.read_excel(uploaded_file,  header=None, names=['position', 'value'], dtype=str)
+    idf = pd.read_excel(uploaded_file,  header=0, names=['position', 'value'], dtype=str)
     dbdf = pd.read_excel('./files/db.xlsx',  header=None, dtype=str)
 
 
@@ -46,11 +56,12 @@ if uploaded_file:
         temp_row, temp_col = temp_df.shape
         input = placeholder.text_input('Iterating over', value=dbdf.iloc[1, i])
         # st.write("checking", dbdf.iloc[1, i])
-        output_dict[dbdf.iloc[1, i]] = ((idf_row + temp_row) - row)/2
+        output_dict[dbdf.iloc[0,i] + " " +  dbdf.iloc[1, i]] = ((idf_row + temp_row) - row)/2
 
     # print(output_dict)
     input = placeholder.text_input('Message', value='Render Complete')
     dict_items = output_dict.items()
     sorted_ans = sorted(dict_items, key = lambda kv: kv[1], reverse=True)
     first_five = list(sorted_ans)[:5]
+    st.write('Top five matches are')
     st.write(first_five)
