3dc2de2318f9e7abb9fd8cb80427c91ff3921668
tuminzee <t.sheth33@gmail.com>
2021-08-13 20:51:49 +0530

    fix type error

--- app.py
+++ app.py
@@ -47,29 +47,36 @@ if uploaded_file:
     output = pd.DataFrame()
     input_headers = []
     for input_col_index in range(INPUT_COL_START_INDEX, INPUT_COL_LENGTH):
-        # input_df.iloc[1: , [0,1]].copy()
         input_headers.append(input_df.iloc[INPUT_HEADER_ROW_INDEX, input_col_index])
         temp_input_df = input_df.iloc[1:, [0,input_col_index]]
+        temp_input_df.dropna(inplace=True)
         temp_input_df.columns = ["position", "value"]
-        # print('temp input', )
-        # print(temp_input_df)
+        temp_input_df['position'] = temp_input_df['position'].astype(float)
+        temp_input_df['position'] = temp_input_df['position'].astype('int64')
+        st.dataframe(temp_input_df.head())
         output_dict = {}
         for db_col_index in range(DB_COL_START_INDEX, DB_COL_LENGTH):
             temp_db_df = db_df.iloc[2: , [0, db_col_index]]
+            
+            temp_db_df.dropna(inplace=True)
             temp_db_df.columns = ["position", "value"]
-            # print('temp db')
-            # print(temp_db_df)
-            df_diff = pd.concat([temp_input_df,temp_db_df]).drop_duplicates(keep=False)
-            diff_row, diff_col = df_diff.shape
-            output_dict[db_df.iloc[DB_HEADER_ROW_INDEX-1, db_col_index] + " " + db_df.iloc[DB_HEADER_ROW_INDEX, db_col_index]] = ((INPUT_ROW_LENGTH-INPUT_ROW_START_INDEX + DB_ROW_LENGTH-DB_ROW_START_INDEX) - diff_row)/2
-        # output_dict[0] =  input_df.iloc[INPUT_HEADER_ROW_INDEX, input_col_index]
+            temp_db_df['position'] = temp_db_df['position'].astype(float)
+            temp_db_df['position'] = temp_db_df['position'].astype('int64')
+
+            # st.dataframe(temp_db_df)  
+
+            ans = temp_input_df.append(temp_db_df, ignore_index=True)
+            output_dict[db_df.iloc[DB_HEADER_ROW_INDEX-1, db_col_index] + " " + db_df.iloc[DB_HEADER_ROW_INDEX, db_col_index]] = int(len(ans)-len(ans.drop_duplicates()))
+
         output = output.append(output_dict, ignore_index=True)
     
     input = placeholder.text_input('Message', value="Completed")
-    # print(input_headers)
+    st.write(input_headers)
     
     output.insert(loc=0, column='Input Variable', value=input_headers)
     st.dataframe(output)
+
+
     downloaded_file = output.to_excel(towrite, encoding='utf-8', index=False, header=True)
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()  # some strings
